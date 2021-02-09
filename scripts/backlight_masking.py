ba924f8a67251e8c1afc90b1c537b21a2b91dc12
Fabian Plum <fabianplum@web.de>
2021-02-09 16:41:38 +0000

    Added scripts to compute mask based on backlight image

---
+++ scripts/backlight_masking.py
@@ -0,0 +1,42 @@
+import cv2
+
+"""
+Alternative masking method, based on backlight images, as described by Stroebel et al. 2018
+"""
+
+
+def generate_backlight_mask(orig_file_loc, backlit_file_loc, threshold=250):
+    orig = cv2.imread(orig_file_loc)
+    backlit = cv2.imread(backlit_file_loc, cv2.IMREAD_GRAYSCALE)
+
+    print("INFO:  Imported images...")
+
+    # binarise
+    ret, image_bin = cv2.threshold(backlit, threshold, 255, cv2.THRESH_BINARY_INV)
+    image_bin[image_bin < 127] = 0
+    image_bin[image_bin > 127] = 1
+
+    mask_name = orig_file_loc[:-4] + '_backlight_masked.png'
+
+    # save as binary png
+    cv2.imwrite(mask_name, image_bin, [cv2.IMWRITE_PNG_BILEVEL, 1])
+
+    print("INFO:  Mask saved to:  ", mask_name)
+
+    # create the image with an alpha channel
+
+    rgba = cv2.cvtColor(orig, cv2.COLOR_RGB2RGBA)
+
+    # assign the mask to the last channel of the image
+    rgba[:, :, 3] = image_bin * 255
+    cutout_name = orig_file_loc[:-4] + '_backlight_cutout.tif'
+    cv2.imwrite(cutout_name, rgba)
+
+    print("INFO:  Cutout saved to:", cutout_name)
+
+
+if __name__ == '__main__':
+    orig_file_loc = "I:\\3D_Scanner\\Manuscript\\Revision\\orig.jpg"
+    backlit_file_loc = "I:\\3D_Scanner\\Manuscript\\Revision\\backlit.jpg"
+
+    generate_backlight_mask(orig_file_loc=orig_file_loc, backlit_file_loc=backlit_file_loc, threshold=250)
---
+++ scripts/calculate_masking_accuracy.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+
+"""
+Script used to compare the masking quality of various methods against a hand annotated "ground truth" binary mask.
+The accuracy is defined as (total_px - (FP + FN)) / total_px
+* total_px = sum of image pixels
+* TN = True Negative (pixels correctly labeled as background)
+* FN = False Negative (pixels incorrectly labeled as background)
+"""
+
+
+def binarise(img):
+    ret, img_bin = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+    img_bin[img_bin < 127] = 0
+    img_bin[img_bin > 127] = 1
+
+    return img_bin
+
+
+def calculate_masking_accuracy(ground_truth, generated_mask):
+    gt = cv2.imread(ground_truth, cv2.IMREAD_GRAYSCALE)
+    gm = cv2.imread(generated_mask, cv2.IMREAD_GRAYSCALE)
+
+    print("INFO:  Imported images...")
+
+    # binarise image
+    gt_bin = binarise(gt)
+    gm_bin = binarise(gm)
+
+    # use type .int16 to avoid overflow when subtracting arrays
+    diff = gt_bin.astype(np.int16) - gm_bin.astype(np.int16)
+    FP_FN = np.sum(np.abs(diff))
+
+    total_px = gt.shape[0] * gt.shape[1]
+
+    print("INFO:  Total number of pixels:", total_px)
+    print("INFO:  Number of incorrect pixels:", FP_FN)
+
+    accuracy = (total_px - FP_FN) / total_px
+
+    return accuracy
+
+
+if __name__ == '__main__':
+    ground_truth_file_loc = "I:\\3D_Scanner\\Manuscript\\Revision\\ground_truth.png"
+    generated_mask_file_loc = "I:\\3D_Scanner\\Manuscript\\Revision\\orig_backlight_masked.png"
+
+    accuracy = calculate_masking_accuracy(ground_truth=ground_truth_file_loc, generated_mask=generated_mask_file_loc)
+    print("INFO:  Masking accuracy:", accuracy)
